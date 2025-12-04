0e6da519b01cf967dd7016cd3b3d8b672424c11e
muzsa2811-web <muzs081128hdgxvba3@cbtis130.edu.mx>
2025-12-04 04:57:17 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@
 $servidor = getenv('MYSQLHOST') ?: 'localhost';
 $usuario = getenv('MYSQLUSER') ?: 'root';
 $clave = getenv('MYSQL_ROOT_PASSWORD') ?: '';
-$base = getenv('MYSQLDATABASE') ?: 'railway';
+$base = getenv('MYSQLDATABASE') ?: 'proyecto_35_37_42_46_54';
 $puerto = getenv('MYSQLPORT') ?: 3306;
 
 // Crear conexión (agregamos el puerto)
@@ -19,3 +19,4 @@ mysqli_set_charset($enlace, "utf8mb4");
 
 // ✅ Conexión exitosa
 ?>
+
